0d2791432493b376b5ee468a6d0f22c0678d6d9d
cgbridgewater <cgbridgewater@outlook.com>
2024-02-19 17:13:39 -0800

    clean up

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom';
 
 const Button = ({ to, text }) => {
+    
     return (
         <Link to={to}>
             <button className="button_link">{text}</button>
