b47a715d0b918b6852ee34cf91d382028af924a9
cgbridgewater <cgbridgewater@outlook.com>
2024-02-19 20:43:07 -0800

    chat pop up and chat box added. Styling will be done remotely for what is inside them

---
+++ src/components/AdmissionsChat.jsx
@@ -0,0 +1,10 @@
+
+
+const AdmissionsChat = () => {
+    
+    return (
+        <iframe className="admissions_chat" src="https://tawk.to/chat/65cb47fe8d261e1b5f5fb79c/1hn1l8mq6" />
+    );
+};
+
+export default AdmissionsChat;
