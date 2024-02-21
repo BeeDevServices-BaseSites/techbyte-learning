826e5a8641ecbe276b1f2ceb16b119c06c5660de
Abrar Hussain <117599985+sahussain887@users.noreply.github.com>
2024-02-21 09:17:33 -0600

    json file added for staff, styled...just a bit

--- src/views/OurHive.jsx
+++ src/views/OurHive.jsx
@@ -1,12 +1,103 @@
+// import { useEffect } from "react";
+// import { changeTitle } from "../utilities/utilityFunctions";
+
+// const OurHive = () => {
+
+//   useEffect(() => {
+//     window.scrollTo(0,0),
+//     changeTitle("Our Hive - ")
+//   },[])
+
+//   return (
+//     <main>
+//       <div className="wrapper">
+//         <div className="large_box">
+//           <h1>Meet our Hive Members</h1>
+//           <div className="row wrap">
+//             <div className="column">
+//               <img src="" alt="" />
+//               <h3>{"Name"}</h3>
+//               <h3>{"Position"}</h3>
+//               <h4>{"Location"}</h4>
+//             </div>
+//             <div className="column">
+//               <img src="" alt="" />
+//               <h3>{"Name"}</h3>
+//               <h3>{"Position"}</h3>
+//               <h4>{"Location"}</h4>
+//             </div>
+//             <div className="column">
+//               <img src="" alt="" />
+//               <h3>{"Name"}</h3>
+//               <h3>{"Position"}</h3>
+//               <h4>{"Location"}</h4>
+//             </div>
+//             <div className="column">
+//               <img src="" alt="" />
+//               <h3>{"Name"}</h3>
+//               <h3>{"Position"}</h3>
+//               <h4>{"Location"}</h4>
+//             </div>
+//             <div className="column">
+//               <img src="" alt="" />
+//               <h3>{"Name"}</h3>
+//               <h3>{"Position"}</h3>
+//               <h4>{"Location"}</h4>
+//             </div>
+//             <div className="column">
+//               <img src="" alt="" />
+//               <h3>{"Name"}</h3>
+//               <h3>{"Position"}</h3>
+//               <h4>{"Location"}</h4>
+//             </div>
+//             <div className="column">
+//               <img src="" alt="" />
+//               <h3>{"Name"}</h3>
+//               <h3>{"Position"}</h3>
+//               <h4>{"Location"}</h4>
+//             </div>
+//             <div className="column">
+//               <img src="" alt="" />
+//               <h3>{"Name"}</h3>
+//               <h3>{"Position"}</h3>
+//               <h4>{"Location"}</h4>
+//             </div>
+//             <div className="column">
+//               <img src="" alt="" />
+//               <h3>{"Name"}</h3>
+//               <h3>{"Position"}</h3>
+//               <h4>{"Location"}</h4>
+//             </div>
+//             <div className="column">
+//               <img src="" alt="" />
+//               <h3>{"Name"}</h3>
+//               <h3>{"Position"}</h3>
+//               <h4>{"Location"}</h4>
+//             </div>
+//             <div className="column">
+//               <img src="" alt="" />
+//               <h3>{"Name"}</h3>
+//               <h3>{"Position"}</h3>
+//               <h4>{"Location"}</h4>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </main>
+//   );
+// };
+
+// export default OurHive;
+
 import { useEffect } from "react";
 import { changeTitle } from "../utilities/utilityFunctions";
+import staffBios from "../utilities/staffBios";
 
 const OurHive = () => {
-
   useEffect(() => {
-    window.scrollTo(0,0),
-    changeTitle("Our Hive - ")
-  },[])
+    window.scrollTo(0, 0);
+    changeTitle("Our Hive - ");
+  }, []);
 
   return (
     <main>
@@ -14,72 +105,24 @@ const OurHive = () => {
         <div className="large_box">
           <h1>Meet our Hive Members</h1>
           <div className="row wrap">
-            <div className="column">
-              <img src="" alt="" />
-              <h3>{"Name"}</h3>
-              <h3>{"Position"}</h3>
-              <h4>{"Location"}</h4>
-            </div>
-            <div className="column">
-              <img src="" alt="" />
-              <h3>{"Name"}</h3>
-              <h3>{"Position"}</h3>
-              <h4>{"Location"}</h4>
-            </div>
-            <div className="column">
-              <img src="" alt="" />
-              <h3>{"Name"}</h3>
-              <h3>{"Position"}</h3>
-              <h4>{"Location"}</h4>
-            </div>
-            <div className="column">
-              <img src="" alt="" />
-              <h3>{"Name"}</h3>
-              <h3>{"Position"}</h3>
-              <h4>{"Location"}</h4>
-            </div>
-            <div className="column">
-              <img src="" alt="" />
-              <h3>{"Name"}</h3>
-              <h3>{"Position"}</h3>
-              <h4>{"Location"}</h4>
-            </div>
-            <div className="column">
-              <img src="" alt="" />
-              <h3>{"Name"}</h3>
-              <h3>{"Position"}</h3>
-              <h4>{"Location"}</h4>
-            </div>
-            <div className="column">
-              <img src="" alt="" />
-              <h3>{"Name"}</h3>
-              <h3>{"Position"}</h3>
-              <h4>{"Location"}</h4>
-            </div>
-            <div className="column">
-              <img src="" alt="" />
-              <h3>{"Name"}</h3>
-              <h3>{"Position"}</h3>
-              <h4>{"Location"}</h4>
-            </div>
-            <div className="column">
-              <img src="" alt="" />
-              <h3>{"Name"}</h3>
-              <h3>{"Position"}</h3>
-              <h4>{"Location"}</h4>
-            </div>
-            <div className="column">
-              <img src="" alt="" />
-              <h3>{"Name"}</h3>
-              <h3>{"Position"}</h3>
-              <h4>{"Location"}</h4>
-            </div>
-            <div className="column">
-              <img src="" alt="" />
-              <h3>{"Name"}</h3>
-              <h3>{"Position"}</h3>
-              <h4>{"Location"}</h4>
-            </div>
+            {staffBios.map((member, index) => (
+              <div className="column" key={index}>
+                <div className="card">
+                  <div className="">
+                    <img src={member.linkToImage} alt={member.name} />
+                    <h3 className="">{member.name}</h3>
+                    <h3>{member.position || "Position TBD"}</h3>
+                  </div>
+                  {/* BACK OF CARD */}
+                  <div className="">
+                    <h4>{"About: " + member.about}</h4>
+                    <h4>{"Location: " + member.location}</h4>
+                    <h4>{"Open to Mentoring/Tutoring: " + member.openToMentoringTutoring}</h4>
+                    <h4>{"Skills: " + member.skills}</h4>
+                  </div>
+                </div>
+              </div>
+            ))}
           </div>
         </div>
       </div>
