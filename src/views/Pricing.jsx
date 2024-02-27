7bc8f3e8a0c4aeccaf9854c8485d71d162475ea4
cgbridgewater <cgbridgewater@outlook.com>
2024-02-26 19:01:24 -0800

    add paypal componenet, insert into pricing and add receipt page

--- src/views/Pricing.jsx
+++ src/views/Pricing.jsx
@@ -1,12 +1,18 @@
 import { useEffect } from "react";
 import { changeTitle } from "../utilities/utilityFunctions";
+import { Link } from "react-router-dom";
+import Paypal from "../components/PayPalButton";
 import full_stack from "../assets/images/full_stack.png"
 import game_dev from "../assets/images/game_dev.jpg"
 import tutor from "../assets/images/tutor.png"
 import best_seller from "../assets/images/best_seller.png"
-import { Link } from "react-router-dom";
+import { PayPalScriptProvider } from "@paypal/react-paypal-js";
+const paypal_keys = import.meta.env.VITE_paypal_keys
 
-const Pricing = () => {
+
+const Pricing = (props) => {
+  const { setDetails } = props;
+
 
   useEffect(() => {
     window.scrollTo(0,0),
@@ -43,7 +49,6 @@ const Pricing = () => {
                   Join us and take your coding skills to the next level!
                 </p>
                 <Link to="/Apply">Apply Here</Link>
-                {/* <a href="#">$8000.00</a> */}
               </div>
             </div>
             {/* END CARD 1 */}
@@ -67,7 +72,6 @@ const Pricing = () => {
                   Join us and embark on an exciting journey of game development in our 2D gaming course today!
                 </p>
                 <Link to="/Apply">Apply Here</Link>
-                {/* <a href="#">$8000.00</a> */}
               </div>
             </div>
             {/* END CARD 2 */}
@@ -75,6 +79,9 @@ const Pricing = () => {
         </div>
         {/* END SECTION 1 */}
         {/* SECTION 2 TITLE */}
+        <PayPalScriptProvider
+          options={{ "client-id": paypal_keys }}
+        >
         <div className="large_box">
           <h2>
             Tutoring Pricing (Bundle And Save!!)
@@ -93,7 +100,8 @@ const Pricing = () => {
               <div className="pricing_card-info">
                 <p className="pricing_card-text">Are you struggling to understand the basics of web development?</p>
                 <p className="pricing_card-text">Sign up for a ½ hour tutoring session where our experienced tutors will work with you at your own pace to help you build a solid foundation in web development.</p>
-                <p className="pricing_card-text">Don't let confusion hold you back from creating amazing websites - sign up for our tutoring session today!</p>                <a href="#">$45.00</a>
+                <p className="pricing_card-text">Don't let confusion hold you back from creating amazing websites - sign up for our tutoring session today!</p>
+                <Paypal base_price={ 30.00 } units={ 1 } description="One 30 minute tuturing session" isOnSale={ false } percent={ 0 } setDetails={ setDetails } /> 
               </div>
             </div>
             {/* END CARD 1 */}
@@ -106,21 +114,23 @@ const Pricing = () => {
               <div className="pricing_card-info">
                 <p className="pricing_card-text">Are you struggling to understand the basics of web development?</p>
                 <p className="pricing_card-text">Sign up for a ½ hour tutoring session where our experienced tutors will work with you at your own pace to help you build a solid foundation in web development.</p>
-                <p className="pricing_card-text">Don't let confusion hold you back from creating amazing websites - sign up for our tutoring session today!</p>                <a href="#">$120.00</a>
+                <p className="pricing_card-text">Don't let confusion hold you back from creating amazing websites - sign up for our tutoring session today!</p>
+                <Paypal base_price={ 30.00 } units={ 3 } description="Three 30 minute tuturing sessions" isOnSale={ true } percent={ 5 } setDetails={ setDetails } /> 
               </div>
             </div>
             {/* END CARD 2 */}
             {/* CARD 3 */}
             <div className="pricing_card">
               <div className="pricing_ribbon">
-                <img src={best_seller} alt="" />
+                <img src={best_seller} alt="best seller" />
                 <div className="pricing_ribbon-text">5x ½ -Hour Sessions</div>
               </div>
               <img src={tutor} alt="tutor"/>
               <div className="pricing_card-info">
                 <p className="pricing_card-text">Are you struggling to understand the basics of web development?</p>
                 <p className="pricing_card-text">Sign up for a ½ hour tutoring session where our experienced tutors will work with you at your own pace to help you build a solid foundation in web development.</p>
-                <p className="pricing_card-text">Don't let confusion hold you back from creating amazing websites - sign up for our tutoring session today!</p>                <a href="#">$175.00</a>
+                <p className="pricing_card-text">Don't let confusion hold you back from creating amazing websites - sign up for our tutoring session today!</p>
+                <Paypal base_price={ 30.00 } units={ 5 } description="Five 30 minute tuturing sessions" isOnSale={ true } percent={ 15 } setDetails={ setDetails } /> 
               </div>
             </div>
             {/* END CARD 3 */}
@@ -134,12 +144,13 @@ const Pricing = () => {
                 <p className="pricing_card-text">Are you struggling to understand the basics of web development?</p>
                 <p className="pricing_card-text">Sign up for a ½ hour tutoring session where our experienced tutors will work with you at your own pace to help you build a solid foundation in web development.</p>
                 <p className="pricing_card-text">Don't let confusion hold you back from creating amazing websites - sign up for our tutoring session today!</p>
-                <a href="#">$300.00</a>
+                <Paypal base_price={ 30.00 } units={ 10 } description="Ten 30 minute tuturing sessions" isOnSale={ true } percent={ 25 } setDetails={ setDetails } /> 
               </div>
             </div>
             {/* END CARD 4 */}
           </div>
         </div>
+        </PayPalScriptProvider>
         {/* END SECTION 2 */}
       </div>
     </main>
---
+++ src/views/Receipt.jsx
@@ -0,0 +1,96 @@
+import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
+import Button from "../components/Button";
+
+
+const Receipt = ({details}) => {
+
+    const navigate = useNavigate();
+
+    // if empty receipt //
+    useEffect(() => {
+        if (details.length === 0) {
+            console.log("NO DATA!!")
+            navigate("/");
+        }
+    }, [details, navigate]);
+
+    return (
+        <div> 
+            {/* ternary checking if details data exists */}
+            { details.length === 0 ?
+                // if empty receipt //
+                null
+                :
+            <main>
+                <div className="wrapper">
+                    {/* recepit container */}
+                    <div className="receipt_container">
+                        <div className='receipt_title'>
+                            <img src="https://cdn.discordapp.com/attachments/1204477744830746655/1208990870171287633/computerBee.png?ex=65e54b7d&is=65d2d67d&hm=e9ce9fda34d8ced8d29a87e9d493dd3295c98e41479f06e32b09490cf5e8fc96&" alt="TechByte Bee" />
+                            <h2>Receipt</h2>
+                        </div>
+                        <div className="receipt_header">
+                            {/* left header */}
+                            <div className="receipt_left">
+                                <h3>TechByte Learning </h3>
+                                <br />
+                                <h5>Invoice#: { details.id }</h5>
+                            </div>
+                            {/* right header */}
+                            <div className="receipt_right">
+                                <h4>Customer Info:</h4>
+                                <h5>{ details.payer.name.given_name } { details.payer.name.surname }</h5>
+                                <h5>{ details.purchase_units[0].shipping.address.address_line_1 }, { details.purchase_units[0].shipping.address.admin_area_2 }, { details.purchase_units[0].shipping.address.admin_area_1 },</h5>
+                                <h5>{ details.purchase_units[0].shipping.address.admin_area_2 }, { details.purchase_units[0].shipping.address.admin_area_1 }, { details.purchase_units[0].shipping.address.postal_code } { details.purchase_units[0].shipping.address.country_code }</h5>
+                                <h5></h5>
+                            </div>
+                        </div>
+                        {/* receipt table*/}
+                        <table className="receipt_table">
+                            <thead>
+                                <tr className="receipt_row">
+                                    <th>Item Desc.</th>
+                                    <th>Each</th>
+                                    <th>Qty</th>
+                                    <th>Total</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                            {
+                                details.purchase_units[0].items.map((item, index) =>{
+                                return(
+                                    <tr key ={index}>
+                                    <td>{item.name}</td>
+                                    <td>${item.unit_amount.value}</td>
+                                    <td>{item.quantity}</td>
+                                    <td>${(item.unit_amount.value * item.quantity).toFixed(2)}</td>
+                                </tr>
+                            )})}
+                                <tr>
+                                    <td>&nbsp;</td>
+                                    <td>&nbsp;</td>
+                                    <td>&nbsp;</td>
+                                    <td>&nbsp;</td>
+                                </tr>
+                                <tr style={{borderTop:"1px solid black"}}>
+                                    <td></td>
+                                    <td></td>
+                                    <td>Total:</td>
+                                    <td><h3>${ details.purchase_units[0].amount.value }</h3> </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                        {/* notes */}
+                        <p>Print and keep this receipt for your records</p>
+                        <h4>Thank You For Joining The TechByte Learning Hive!</h4>
+                    </div>
+                    <Button to="/" text="CLOSE" />
+                </div>
+            </main>
+            }
+        </div>
+    );
+}
+
+export default Receipt;
