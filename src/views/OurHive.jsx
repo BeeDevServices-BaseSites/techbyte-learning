949edbb95bfa70b8f931ab5521de112a7e83469d
cgbridgewater <cgbridgewater@outlook.com>
2024-03-11 23:34:09 -0700

    style and layout

--- src/views/OurHive.jsx
+++ src/views/OurHive.jsx
@@ -9,20 +9,17 @@ const OurHive = () => {
   return (
     <main>
       <div className="wrapper">
-        <div className="split_box our_hive_split_box">
+        <div className="split_box">
           <div className="box_left_text">
-            <h2>Hive Staff</h2>
+            <h2>Meet Our Hive</h2>
             <p><span>W</span>hether guiding students through the latest programming languages, unlocking the secrets of cutting-edge technologies, or fostering innovative problem-solving skills, the TechByte Learning staff is committed to empowering learners with the tools they need to succeed in todays fast-paced digital landscape. Get ready to learn from the best and embark on a transformative journey with our hive of skilled instructors.</p>
           </div>
           <div className="box_right_img">
             <img src={ staff_bios_image } alt="Instructor" />
           </div>
         </div>
+        <StaffCard card_data = { card_data }  show_tech = { false } />
       </div>
-      <div>
-        <h2 className="our_hive_h2">Meet Our Hive</h2>
-      </div>
-      <StaffCard card_data = { card_data }  show_tech = { false } />
     </main>
   );
 };
