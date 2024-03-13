ea197d6b36309cb31deb47adfb30db640cbce5a8
cgbridgewater <cgbridgewater@outlook.com>
2024-03-12 20:06:18 -0700

    refactor course cards for json use and reqorking receipt pages

---
+++ src/components/PriceTags.jsx
@@ -0,0 +1,90 @@
+import { Link } from "react-router-dom";
+import Paypal from "../components/PayPalButton";
+import full_stack from "../assets/images/full_stack.png"
+import game_dev from "../assets/images/game_dev.jpg"
+import tutor from "../assets/images/tutor.png"
+import best_seller from "../assets/images/best_seller.png"
+import { PayPalScriptProvider } from "@paypal/react-paypal-js";
+
+const PriceTags = ({ tag_data , setDetails }) => {
+
+
+    const initialOptions = {
+        "client-id": "AboCjdkH7teR_lvYiywVhF41PTL0gUZ6UM0Uz5mTf3BOaFaJTh_njS1PrDz0wljMB5MYOv7PvYu1NvU0",
+        "enable-funding": "venmo,card",
+        "disable-funding": "paylater",
+        "data-sdk-integration-source": "integrationbuilder_sc",
+    };
+
+    return (
+
+        <PayPalScriptProvider options={ initialOptions } >
+            <div className="pricing_container">
+                {/* DEV CARD 1 */}
+                {tag_data.map((price_tag) => (
+                    
+                    <div className="pricing_card" key={ price_tag.id }>
+
+                        {/* PRICE RIBBON W/ TEXT */}
+                        <div className="pricing_ribbon">
+                            {price_tag.is_popular ? 
+                            <img src="https://live.staticflickr.com/65535/53583339767_1406c80170_n.jpg" alt="best seller" />
+                            :""
+                            }
+                            <div className="pricing_ribbon_text"> { price_tag.title }</div>
+                        </div>
+
+                        {/* PRICE TAG IMAGE */}
+                        { price_tag.image == "" ? 
+                            <img src="https://doceri.thinkific.com/assets/defaults/default-product-card.png" alt="web dev card"/>
+                        :
+                            <img src={ price_tag.image } alt={ price_tag.title } />
+                        }
+
+                        {/* START DATE */}
+                        { price_tag.start_date !== "" ?
+                            <>
+                                <br />
+                                <h4>Course Start Date: { price_tag.start_date }</h4>
+                            </>
+                        :
+                            ""
+                        }
+
+                        {/* COURSE SUMMARY */}
+                        <div className="pricing_card_info">
+                            { price_tag.summary.map((card_summary, idx) => (
+                                <p className="pricing_card_text" key={ idx }>
+                                    { card_summary }
+                                </p>
+                            ))}
+
+                            {/* PRICE TAG */}
+                            { price_tag.has_checkout ? 
+                                <Paypal
+                                    base_price={ price_tag.price }
+                                    units={ price_tag.units }
+                                    description= { price_tag.receipt_description }
+                                    isOnSale={ price_tag.is_on_sale }
+                                    percent={ price_tag.percent }
+                                    setDetails={ setDetails } 
+                                    is_tutoring = { price_tag.is_tutoring }
+                                />
+                            :
+                                <div className="price_tag">
+                                    <h4>${ price_tag.price.toFixed(2) }</h4>
+                                    <Link to="/Apply">Apply Here</Link>
+                                </div>
+                            }
+
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </PayPalScriptProvider>
+
+
+    )
+}
+
+export default PriceTags;
---
+++ src/views/Services.jsx
@@ -0,0 +1,92 @@
+import { useEffect } from "react";
+import { changeTitle } from "../utilities/utilityFunctions";
+import Button from "../components/Button";
+import PriceTags from "../components/PriceTags";
+import pricing_info from "../assets/json/course_cards.json"
+
+const Services = ({ setDetails }) => {
+
+  const mini_course_data = pricing_info.filter(price => price.is_active).filter(price => price.is_mini_course);
+  const dev_course_data = pricing_info.filter(price => price.is_active).filter(price => price.is_full_course);
+  const tutoring_data = pricing_info.filter(price => price.is_active).filter(price => price.is_tutoring);
+
+  useEffect(() => {
+    window.scrollTo(0,0),
+    changeTitle("TechByte Services - ")
+  },[])
+
+  const handleClick = (e, id) => {
+    e.preventDefault();
+    const element = document.getElementById(id);
+    window.scrollTo({
+        top: element.offsetTop -150,
+        behavior: 'smooth'
+    });
+}
+
+  return (
+    <main>
+      <div className="wrapper">
+        <div className="large_box">
+          <div className="service_links">
+            <h2>
+              TechByte Learning Services
+            </h2>
+            <div className="service_jump_link">
+              <p href="#devcourses" onClick={(e) => handleClick(e, "devcourses")}>|&nbsp;Development&nbsp;|</p>
+              <p href="#minisessons" onClick={(e) => handleClick(e, "minisessions")}>|&nbsp;Mini&nbsp;Sessions&nbsp;|</p>
+              <p onClick={(e) => handleClick(e, "tutoring")}>|&nbsp;Tutoring&nbsp;|</p>
+            </div>
+          </div>
+
+          {/* SECTION 1 */}
+          {/* TITLE */}
+          <h2 id="devcourses">
+            TechByte Development Programs
+          </h2>
+          <br />
+          {/* FULL DEV COURSE PRICE TAGS */}
+          <PriceTags  tag_data={ dev_course_data } setDetails={ setDetails } />
+
+          {/* END SECTION 1 */}
+
+          {/* SECTION 2 */}
+          {/* TITLE */}
+          <h2 id="minisessions">
+            TechByte Mini Sessions
+          </h2>
+          <br />
+          {/* MINI COURSE PRICE TAGS */}
+          <PriceTags tag_data={ mini_course_data } setDetails={ setDetails } />
+          {/* END SECTION 2 */}
+
+          {/* SECTION 3 */}
+          {/* TITLE */}
+          <h2 id="tutoring">
+            Techbyte Tutoring
+          </h2>
+          <h4>
+            Not sure what you need, or have questions about TechByte Tutoring?
+          </h4>
+          <h4>
+            Contact <a href="mailto:tutoring@techbyte-learning.com?subject=Tutoring%20Inquiry%20">tutoring@techbyte-learning.com</a>
+          </h4>
+          <br />
+          <h5>
+            Don't forget to take advantage of our bundle pricing options.
+          </h5>
+          <br />
+          {/* TUTORING PRICE TAGS */}
+          <PriceTags tag_data={ tutoring_data } setDetails={ setDetails } />
+          {/* END SECTION 3 */}
+        </div>
+        <h4>
+          Already purchased a tutoring session? 
+        </h4>
+        <Button to="/booktutoring" text="Schedule Tutoring" />
+      </div>
+    </main>
+  );
+};
+
+export default Services;
