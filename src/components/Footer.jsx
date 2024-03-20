909201f7ecfd66b7c8a9f7cecd69980fef2cc501
cgbridgewater <cgbridgewater@outlook.com>
2024-03-20 13:27:49 -0700

    footer changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,18 +4,42 @@ import woman_owned from "../assets/images/woman_owned.png"
 const Footer = () => {
   return (
     <footer>
-      <div className="footer_text">
-        <p>
-          Website Built By: 
-        </p>
-        <a href="https://beedev-services.com/" target="_blank">
-          BEEDEV-SERVICES
-        </a>
+      <div className="footer_links">
+
+        <div className="footer_text">
+
+            <p>
+              Website Built By: 
+            </p>
+            <p>
+              <a href="https://beedev-services.com/" target="_blank">
+                BEEDEV-SERVICES
+              </a>
+            </p>
+
+        </div>
+
+        <div className="footer_text">
+          <p>
+            <a href="https://techbyte-learning.com/bytewise/" target="_blank">
+              ByteWise Dispatch
+            </a>
+          </p>
+          <p>
+            <a href="https://learn.techbyte-learning.com/" target="_blank">
+              Student Login
+            </a>
+          </p>
+        </div>
+
       </div>
+
+
       <div className="footer_images">
         <img className="footer_image_one" src={ vet_owned } alt="vet owned" />
         <img className="footer_image_two" src={ woman_owned } alt="vet owned" />
       </div>
+
     </footer>
   )
 };
