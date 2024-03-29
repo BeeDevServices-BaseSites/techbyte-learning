0623f326ba39c1a2756d79017cdffc8ed202da30
cgbridgewater <cgbridgewater@outlook.com>
2024-03-29 16:02:22 -0700

    adding content and colors to the 'ourtechs' section

---
+++ src/components/CSS.jsx
@@ -0,0 +1,67 @@
+
+const CSS = () => {
+
+    return (
+        <>
+            <h2>
+                What is CSS?
+            </h2>
+            <p>
+            CSS stands for Cascading Style Sheets and it is a language used to describe the presentation of a website or webpage. In simpler terms, CSS is what makes a website look visually appealing by controlling and styling elements like fonts, colors, layouts, and spacing. It allows web designers to customize the appearance of their website without changing the actual content. CSS works in conjunction with HTML (the structure of a webpage) to create the overall design and layout of a website.
+            </p>
+            <br />
+            {/* HTML EXAMPLE */}
+            <h5>
+                Lets add some CSS to our HTML example:
+            </h5>
+            <div className="code_box">
+                <code>
+                    <p>
+                        h1 &#123;<br/>
+                        &nbsp;&nbsp;color: darkred;<br/>
+                        &#125;;<br/>
+                    </p>
+                    <p>
+                        p &#123;<br/>
+                        &nbsp;&nbsp;color: goldenrod;<br/>
+                        &nbsp;&nbsp;font-weight: bold;<br/>
+                        &#125;;<br/>
+                    </p>
+                    <p>
+                        img &#123;<br/>
+                        &nbsp;&nbsp;border: 2px solid red;<br/>
+                        &#125;;<br/>
+                    </p>
+                    <p>
+                        a &#123;<br/>
+                        &nbsp;&nbsp;color: green;<br/>
+                        &#125;;<br/>
+                    </p>
+                    <p>
+                        a:hover &#123;<br/>
+                        &nbsp;&nbsp;color: red;<br/>
+                        &#125;;<br/>
+                    </p>
+                </code>
+            </div>
+            {/* END EXAMPLE SECTION */}
+
+            {/* HTML RESULT SECTION */}
+            <h5>
+                Here is the result:
+            </h5>
+            <div className="code_box_result css">
+                <h1>Welcome to TechByte Learning</h1>
+                <p>This is a paragraph of text on my website.</p>
+                <img src="https://cataas.com/cat/gif" alt="Random Cat Image"/>
+                <a href="https://cataas.com/cat" target="_blank">Visit A Random Cat Image</a>
+            </div>
+            {/* END RESULT SECTION */}
+            <p>
+            By adding specific CSS code to our HTML example, we can create a visual apeal to our website with customized elements such as colors, fonts, and borders.
+            </p>
+        </>
+    )
+};
+
+export default CSS;
--- src/views/OurTechs.jsx
+++ src/views/OurTechs.jsx
@@ -1,19 +1,16 @@
+import CSS from "../components/CSS"
+import HTML5 from "../components/HTML5"
 
 const OurTechs = () => {
 
 
     return(
         <div className="wrapper">
-            <div className="large_box">
-                <h2 id="CSS">
-                    What is HTML?
-                </h2>
-                <p>
-                    <span>H</span>TML, which stands for Hypertext Markup Language, is a standardized system used for creating and structuring web pages and web applications. It consists of a series of elements, or tags, that define the different parts of a webpage, such as headings, paragraphs, images, links, and forms. HTML is the foundation of all web development and is essential for creating a visually appealing and functional website. It works in conjunction with other web technologies, such as CSS for styling and JavaScript for interactivity, to create dynamic and engaging web content.
-                </p>
-                <p>
-                In HTML, each tag is enclosed in angle brackets, with the opening tag defining the beginning of an element and the closing tag indicating the end. Attributes can also be added to tags to provide additional information or functionality to an element. By using a combination of HTML tags and attributes, web developers can create a structure and design for a webpage that is easily interpreted by web browsers. Ultimately, HTML is a crucial tool for anyone looking to build a website or web application, as it provides the framework for organizing and presenting content on the internet.
-                </p>
+            <div id="language_HTML" className="large_box">
+                <HTML5 />
+            </div>
+            <div id="language_CSS" className="large_box">
+                <CSS />
             </div>
         </div>
     )
